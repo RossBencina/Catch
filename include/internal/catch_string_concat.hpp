73060cbb37622dfa0ff90efe45e5f08608c197ac
Ross Bencina <rossb@audiomulch.com>
2016-03-20 12:20:04 +1100

    fix copyright date

--- include/internal/catch_string_concat.hpp
+++ include/internal/catch_string_concat.hpp
@@ -1,6 +1,6 @@
 /*
 *  Created by Ross on 20/03/2016.
-*  Copyright 2011 Two Blue Cubes Ltd. All rights reserved.
+*  Copyright 2016 Two Blue Cubes Ltd. All rights reserved.
 *
 *  Distributed under the Boost Software License, Version 1.0. (See accompanying
 *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
